0b8c5ea6d45af71fcdc9c3cd8db17c87e58724c6
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-07-12 00:55:28 -0300

    publishing

---
+++ bot2.js
@@ -0,0 +1,260 @@
+require('dotenv').config({ path: 'slack' });
+
+var Botkit = require('botkit');
+var express = require('express');
+const https = require('https');
+var fs = require('fs')
+var path = require('path')
+const { exec } = require('child_process');
+
+// Configure your bot.
+var slackController = Botkit.slackbot({ clientSigningSecret: process.env.SLACK_SIGNING_SECRET });
+var slackBot = slackController.spawn({
+    token: process.env.SLACK_TOKEN
+});
+// slackController.hears(['.*'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+slackController.hears(['.*'], ['direct_message', 'direct_mention', 'other_event', 'file_shared'], function (bot, message) {
+    slackController.log('Slack message received');
+    console.log('message', message);
+    // bot.reply(message, {
+    //     attachments: [
+    //         {
+    //             title: 'Please select your Geography',
+    //             attachment_type: 'default',
+    //             actions: [
+    //                 {
+    //                     "name": "eu",
+    //                     "text": "eu",
+    //                     "value": "eu",
+    //                     "type": "button",
+    //                 },
+    //                 {
+    //                     "name": "us-south",
+    //                     "text": "us-south",
+    //                     "value": "us-south",
+    //                     "type": "button",
+    //                 },
+    //                 {
+    //                     "name": "test",
+    //                     "text": "test",
+    //                     "value": "test",
+    //                     "type": "button",
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // });
+    bot.reply(message, {
+        "actions": [
+            {
+                "name": "games_list",
+                "text": "Pick a game...",
+                "type": "select",
+                "options": [
+                    {
+                        "text": "Chess",
+                        "value": "chess"
+                    },
+                    {
+                        "text": "Falken's Maze",
+                        "value": "maze"
+                    },
+                    {
+                        "text": "Global Thermonuclear War",
+                        "value": "war"
+                    }
+                ]
+            }
+        ]
+    })
+    // bot.reply(message, "I'm here :) :hello-bear:");
+    // bot.say({
+    //     text: "Invalid file name :sad1: ",
+    //     channel: message.channel_id // channel Id for #slack_integration
+    // });
+    // bot.send({})
+    // bot.reply(message,"fdafda")
+    // bot.say({
+    //     text: "Invalid file name :sad1: ",
+    //     channel: message.channel_id 
+    // });
+    // if (message.text == "collect") {
+    //     bot.replyInThread(message, ':waitingmaas: Give me some time to get all the information from Grafana :construction-2:')
+    //     exec('node server.js', (err, stdout, stderr) => {
+    //         console.log(stderr);
+    //         console.log(stdout);
+    //         bot.replyInThread(message, '<@'+bot.identity.id+'> Here are the reports')
+    //     })
+    // }
+
+    // if (message.text.includes("reports")) {
+    //     // bot.replyInThread(message, 'Here are the reports')
+    //     uploadTheFiles(bot, message, ["ge4Dashboard/ge4-dashboard.png",
+    //         "ge4MonthEnd/ge4Month-Infra.png",
+    //         "ge4MonthEnd/ge4Month-Checklist.png",
+    //         "ge4MonthEnd/ge4Month-Oracle.png",
+    //         "ge4MonthEnd/ge4Month-HanaHa4.png",
+    //         "ge4MonthEnd/ge4MonthEnd-stoBrSoftlayerConnectivity.png",
+    //         "ge4MonthEnd/ge4Month-HortoCoreSwitch.png"
+    //     ])
+    // }
+
+
+    // exec('chmod -R 777 criar_planilha.js', (err, stdout, stderr) => { console.log(stderr) });
+    // exec('chmod -R 777 bot.js', (err, stdout, stderr) => { console.log(stderr) });
+    // exec('chmod -R 777 *.*', (err, stdout, stderr) => { console.log(stderr) });
+    // bot.send("Shutting down VM #34324....", "fdsafdsafdsa");
+    // bot.send("Shutting down VM #34324....")
+    // var slackMessage = 
+    // middleware.interpret(bot, message, function() {
+    //   if (message.watsonError) {
+    //     console.log(message.watsonError);
+    //     bot.reply(message, message.watsonError.description || message.watsonError.error);
+    //   } else if (message.watsonData && 'output' in message.watsonData) {
+    //     bot.reply(message, "fdsafdsafdsa34243kj2n4jkn43jkndjksnfdsakjnfdjksanfkjdsn");
+    //   } else {
+    //     console.log('Error: received message in unknown format. (Is your connection with Watson Assistant up and running?)');
+    //     bot.reply(message, 'I\'m sorry, but for technical reasons I can\'t respond to your message');
+    //   }
+});
+
+async function uploadTheFiles(bot, message, images) {
+    var i = 0;
+    while (i < images.length) {
+        bot.api.files.upload({
+            file: fs.createReadStream(images[i]),
+            filename: images[i],
+            filetype: "png",
+            channels: message.channel,
+            thread_ts: message.thread_ts
+        }, function (err, res) {
+            if (err) {
+                console.log("Failed to add file :(", err)
+                bot.reply(message, 'Sorry, there has been an error: ' + err)
+            }
+        })
+        i++;
+    }
+}
+// slackController.on('file_shared', function (bot, message) {
+
+//     bot.api.files.info({ file: message.file_id }, (err, response) => {
+//         // console.log(response.file.title)
+//         console.log(response.file.filetype)
+//         if (response.file.title == "config_criar_planilha") {
+//             const file = fs.createWriteStream(path.join(__dirname, "config_criar_planilha"));
+//             console.log(path.join(__dirname, "config_criar_planilha"))
+//             https.get(response.file.url_private_download, {
+//                 headers: {
+//                     'Authorization': 'Bearer ' + process.env.SLACK_TOKEN
+//                 }
+//             }, function (response) {
+//                 response.pipe(file);
+//                 console.log(fs.existsSync(path.join(__dirname, "config_criar_planilha")))
+//             });
+//             bot.say({
+//                 text: "Config file for 'Criar planilha' Received! :fbhappy:",
+//                 channel: message.channel_id // channel Id for #slack_integration
+//             });
+//         } else if (response.file.title == "config") {
+//             const file = fs.createWriteStream(path.join(__dirname, "config"));
+//             https.get(response.file.url_private_download, {
+//                 headers: {
+//                     'Authorization': 'Bearer ' + process.env.SLACK_TOKEN
+//                 }
+//             }, function (response) {
+//                 response.pipe(file);
+//             });
+//             bot.say({
+//                 text: "Config file for all the scripts Received! :fbhappy:",
+//                 channel: message.channel_id // channel Id for #slack_integration
+//             });
+//         } else if (response.file.filetype == "csv") {
+//             if (response.file.title.includes("metrics_")) {
+//                 var output_filename = response.file.title;
+//                 output_filename = output_filename.split(".")[0];
+//                 output_filename = output_filename.split("metrics_");
+//                 output_filename = output_filename[1].toString().trim().split("-")
+//                 output_filename = output_filename[2] + "-" + output_filename[1] + "-" + output_filename[0]
+//                 output_filename = "Metricas_" + output_filename + ".xlsx"
+//                 const file = fs.createWriteStream(path.join(__dirname, "a.csv"))
+//                 https.get(response.file.url_private_download, {
+//                     headers: {
+//                         'Authorization': 'Bearer ' + process.env.SLACK_TOKEN
+//                     }
+//                 }, function (response) {
+//                     response.pipe(file);
+//                     build(bot, message, output_filename);
+//                 });
+//                 bot.say({
+//                     text: "Received! :fbhappy: \nProcessing file and collecting metrics... :construction-2:",
+//                     channel: message.channel_id // channel Id for #slack_integration
+//                 });
+//             } else {
+//                 bot.say({
+//                     text: "Invalid file name :sad1: ",
+//                     channel: message.channel_id // channel Id for #slack_integration
+//                 });
+//             }
+
+//         }
+
+//     })
+// });
+
+slackBot.startRTM();
+
+// function build(bot, message, output_filename) {
+//     // exec('node criar_planilha.js', (err, stdout, stderr) => {
+//     //     console.log('aaa');
+//     //     console.log(stderr);
+//     //     console.log(stdout);
+//     //     exec('ls -la', (err, stdout, stderr) => {
+//     //         console.log('lista de arquivos')
+//     //         console.log(stdout)
+//     //     })
+
+//     // })
+//     // console.log(message.channel)
+//     // cmd.exe /c executar_todos.bat
+//     exec('make build', (err, stdout, stderr) => {
+//         console.log(stdout)
+//         fs.readFile("config", { encoding: 'utf-8' }, function (err, data) {
+//             var file_name = data.split('\n')[2].replace("OUTPUT_FILE=", "")
+//             if (fs.existsSync(path.join(__dirname, file_name))) {
+//                 bot.say({
+//                     text: "Finished, uploading the file...",
+//                     channel: message.channel_id // channel Id for #slack_integration
+//                 });
+//                 bot.api.files.upload({
+//                     file: fs.createReadStream(path.join(__dirname, file_name)),
+//                     filename: output_filename,
+//                     filetype: "xlsx",
+//                     channels: message.channel_id,
+
+//                 }, function (err, res) {
+//                     if (err) {
+//                         console.log("Failed to add file :(", err)
+//                         bot.reply(message, 'Sorry, there has been an error: ' + err)
+//                     }
+//                 })
+//             } else {
+//                 console.log('file dont exists', path.join(__dirname, file_name))
+//             }
+//         });
+
+
+//     });
+
+// }
+
+// // Create an Express app
+var app = express();
+var port = process.env.PORT || 5000;
+app.set('port', port);
+app.get('/', (req, res) => {
+    res.send('working')
+})
+app.listen(port, function () {
+    console.log('Client server listening on port ' + port);
+});
---
+++ server.js
@@ -0,0 +1,262 @@
+require('dotenv').config({ path: 'config_grafana' })
+const pptrFirefox = require('puppeteer');
+var Jimp = require('jimp');
+var browser, page;
+
+const URL_GRAFANA = process.env.URL_GRAFANA
+const USERNAME_GRAFANA = process.env.USERNAME_GRAFANA
+const PASSWORD_GRAFANA = process.env.PASSWORD_GRAFANA
+
+async function main() {
+    browser = await pptrFirefox.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], ignoreHTTPSErrors: true, waitUntil: ['load', 'domcontentloaded'] });
+    page = await browser.newPage();
+
+    await page.setViewport({ width: 1466, height: 1100 });
+    await page.goto(URL_GRAFANA + '/login');
+    await page.type("[name='username']", USERNAME_GRAFANA)
+    await page.type("[name='password']", PASSWORD_GRAFANA)
+    await page.evaluate(() => {
+        document.querySelectorAll('button')[0].click()
+        return "foi"
+    })
+    
+    await page.waitFor(1000)
+
+    console.log('ge4Dashboard...')
+    await ge4Dashboard(page)
+
+    console.log('ge4MonthEnd...')
+    await ge4MonthEnd(page)
+
+    console.log('ge4DailyMajor...')
+    await ge4DailyMajor(page)
+
+    console.log('ge4UnixServerDetails...');
+    await ge4UnixServerDetails(page)
+
+    console.log('ge4NetworkStatus...');
+    await ge4NetworkStatus(page)
+
+
+    await browser.close();
+}
+
+async function ge4Dashboard(page) {
+    await page.goto(URL_GRAFANA + '/d/r48TsDGZk/ge4-dashboard?refresh=1m&orgId=1');
+    await page.waitFor(5000)
+    await page.screenshot({ path: 'ge4Dashboard/ge4-dashboard.png' });
+    return true;
+}
+
+async function ge4MonthEnd(page) {
+    await page.goto(URL_GRAFANA + '/d/AbCPWY9ik/gerdau-month-end-dashboard-v1?refresh=5m&orgId=1');
+    await page.setViewport({ width: 1466, height: 4100 });
+    await page.waitFor(12000)
+    await page.screenshot({ path: 'ge4MonthEnd/ge4bruto.png' });
+
+    Jimp.read('ge4MonthEnd/ge4bruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 70, 1400, 870)
+            .quality(100)
+            .write('ge4MonthEnd/ge4Month-Infra.png');
+    });
+
+    Jimp.read('ge4MonthEnd/ge4bruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 890, 1400, 1090)
+            .quality(100)
+            .write('ge4MonthEnd/ge4Month-Checklist.png');
+    });
+
+    Jimp.read('ge4MonthEnd/ge4bruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 1950, 1400, 600)
+            .quality(100)
+            .write('ge4MonthEnd/ge4Month-Oracle.png');
+    });
+
+    Jimp.read('ge4MonthEnd/ge4bruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 2500, 1400, 320)
+            .quality(100)
+            .write('ge4MonthEnd/ge4Month-HanaHa4.png');
+    });
+
+    Jimp.read('ge4MonthEnd/ge4bruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 2800, 1400, 460)
+            .quality(100)
+            .write('ge4MonthEnd/ge4MonthEnd-stoBrSoftlayerConnectivity.png');
+    });
+
+    Jimp.read('ge4MonthEnd/ge4bruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 3240, 1400, 250)
+            .quality(100)
+            .write('ge4MonthEnd/ge4Month-HortoCoreSwitch.png');
+    });
+    return true;
+}
+
+async function ge4DailyMajor(page) {
+    await page.goto(URL_GRAFANA + '/d/N4fCZ_kZz/daily-major-environments-overview?orgId=1');
+    await page.setViewport({ width: 1466, height: 4100 });
+    await page.waitFor(2000)
+    await page.evaluate(() => {
+        while (document.querySelectorAll('[class="fa fa-chevron-right"]').length > 0) {
+            document.querySelectorAll('[class="fa fa-chevron-right"]')[0].click()
+        }
+        return "foi"
+    })
+    await page.waitFor(20000)
+    await page.screenshot({ path: 'ge4DailyMajor/ge4DailyMajorbruto.png' });
+
+    Jimp.read('ge4DailyMajor/ge4DailyMajorbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 70, 1400, 670)
+            .quality(100)
+            .write('ge4DailyMajor/ge4DailyMajor-pn4.png');
+    });
+
+    Jimp.read('ge4DailyMajor/ge4DailyMajorbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 720, 1400, 670)
+            .quality(100)
+            .write('ge4DailyMajor/ge4DailyMajor-nf4.png');
+    });
+
+    Jimp.read('ge4DailyMajor/ge4DailyMajorbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 1390, 1400, 870)
+            .quality(100)
+            .write('ge4DailyMajor/ge4DailyMajor-ge4.png');
+    });
+
+    Jimp.read('ge4DailyMajor/ge4DailyMajorbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 2230, 1400, 320)
+            .quality(100)
+            .write('ge4DailyMajor/ge4DailyMajor-hana_ha4.png');
+    });
+
+
+    return true;
+}
+
+async function ge4UnixServerDetails(page) {
+    await page.goto(URL_GRAFANA + '/d/c8zN_bZWk/unix-server-details?refresh=15m&orgId=1');
+    await page.setViewport({ width: 1466, height: 4100 });
+    await page.waitFor(2000)
+    await page.evaluate(() => {
+        while (document.querySelectorAll('[class="fa fa-chevron-right"]').length > 0) {
+            document.querySelectorAll('[class="fa fa-chevron-right"]')[0].click()
+        }
+        return "foi"
+    })
+    await page.waitFor(30000)
+    await page.screenshot({ path: 'ge4UnixServerDetails/ge4UnixServerDetailsbruto.png' });
+
+    Jimp.read('ge4UnixServerDetails/ge4UnixServerDetailsbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 70, 1400, 570)
+            .quality(100)
+            .write('ge4UnixServerDetails/ge4UnixServerDetails-diskio.png');
+    });
+
+    Jimp.read('ge4UnixServerDetails/ge4UnixServerDetailsbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 650, 1400, 570)
+            .quality(100)
+            .write('ge4UnixServerDetails/ge4UnixServerDetails-filesystem.png');
+    });
+
+    Jimp.read('ge4UnixServerDetails/ge4UnixServerDetailsbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 1150, 1400, 1000)
+            .quality(100)
+            .write('ge4UnixServerDetails/ge4UnixServerDetails-utilization.png');
+    });
+
+    return true;
+}
+
+async function ge4NetworkStatus(page) {
+    await page.goto(URL_GRAFANA + '/d/66UW_bWWz/network-status?refresh=5s&orgId=1');
+    await page.setViewport({ width: 1466, height: 4100 });
+    await page.waitFor(2000)
+    await page.evaluate(() => {
+        while (document.querySelectorAll('[class="fa fa-chevron-right"]').length > 0) {
+            document.querySelectorAll('[class="fa fa-chevron-right"]')[0].click()
+        }
+        return "foi"
+    })
+    await page.waitFor(30000)
+    await page.screenshot({ path: 'ge4NetworkStatus/ge4NetworkStatusbruto.png' });
+
+    Jimp.read('ge4NetworkStatus/ge4NetworkStatusbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 70, 1400, 770)
+            .quality(100)
+            .write('ge4NetworkStatus/ge4NetworkStatus-tunnelstatus.png');
+    });
+
+    Jimp.read('ge4NetworkStatus/ge4NetworkStatusbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 830, 1400, 270)
+            .quality(100)
+            .write('ge4NetworkStatus/ge4NetworkStatus-fromIBMCloudSAO01.png');
+    });
+
+    Jimp.read('ge4NetworkStatus/ge4NetworkStatusbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 1080, 1400, 270)
+            .quality(100)
+            .write('ge4NetworkStatus/ge4NetworkStatus-fromIBMCloudWDC04.png');
+    });
+
+    Jimp.read('ge4NetworkStatus/ge4NetworkStatusbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 1320, 1400, 460)
+            .quality(100)
+            .write('ge4NetworkStatus/ge4NetworkStatus-hortorouters.png');
+    });
+
+    Jimp.read('ge4NetworkStatus/ge4NetworkStatusbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 1760, 1400, 380)
+            .quality(100)
+            .write('ge4NetworkStatus/ge4NetworkStatus-twslatency.png');
+    });
+
+
+    Jimp.read('ge4NetworkStatus/ge4NetworkStatusbruto.png', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            .crop(70, 2100, 1400, 270)
+            .quality(100)
+            .write('ge4NetworkStatus/ge4NetworkStatus-tsmlatency.png');
+    });
+
+    return true;
+}
+
+
+main();
